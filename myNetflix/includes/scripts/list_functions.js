800f1e4db41be925f4952e22ef797ebe72581958
teddy0044 <55285234+teddy0044@users.noreply.github.com>
2019-11-29 12:18:07 +0100

    saisons

--- myNetflix/includes/scripts/list_functions.js
+++ myNetflix/includes/scripts/list_functions.js
@@ -21,7 +21,7 @@ function getListeSeries(table){
 				var cellSaisons = row.insertCell(2);
 				cellnom.innerText = lesSeries[i].nom;
 				cellannee.innerText = lesSeries[i].anneeparution;
-				cellSaisons.innerHTML = "<button id='saisons' value='"+ lesSeries[i].id +"'>Voir Saisons</button>";
+				cellSaisons.innerHTML = "<button id='saisons' onclick='voirSaisons("+ lesSeries[i].id +")'>Voir Saisons</button>";
 				//getNbSaisons(lesSeries[i].id, cellSaisons);
 				row.setAttribute("tag", lesSeries[i].id);
 				cellnom.style.textAlign = "left";
@@ -37,6 +37,13 @@ function getListeSeries(table){
 	xmlHttpSeries.send();
 }
 
+function voirSaisons(id) {
+	document.getElementById("Saisons").style.display = 'block';
+	var tableSaisons = document.querySelector("#tbSaisons>tbody");
+	getSaisons(tableSaisons);
+}
+
+
 function getSaisons(id){
 	console.log(id);
 	var xmlHttpSaisons = getAjaxRequestObject();
@@ -47,11 +54,42 @@ function getSaisons(id){
 			}catch (e){
 				console.log('Pas de saisons');
 			}
+			var text= this.responseText;
+			var lesSaisons = JSON.parse(text);
+			lesSaisons.sort(function(a, b){
+				if (a.id < b.id){
+					return -1;
+				}else if (a.id > b.id){
+					return 1;
+				}else{
+					return 0;
+				}
+			});
+
+			for(var i=0;i<lesSaisons.length;i++){
+				var row = table.insertRow(i);
+				var cellnom = row.insertCell(0);
+				var cellannee = row.insertCell(1);
+				var cellSaisons = row.insertCell(2);
+				cellnom.innerText = lesSaisons[i].nom;
+				cellannee.innerText = lesSaisons[i].anneeparution;
+				cellSaisons.innerHTML = "<button id='saisons' value='"+ lesSaisons[i].id +"'>Voir Episode</button>";
+				//getNbSaisons(lesSeries[i].id, cellSaisons);
+				row.setAttribute("tag", lesSaisons[i].id);
+				cellnom.style.textAlign = "left";
+				cellnom.style.paddingLeft = "10px";
+
+				/*row.onclick = function(){
+					getSaisons(this.getAttribute("tag"));
+				}*/
+			}
 		}
 	};
 	xmlHttpSaisons.open("GET", "../api-netflix/api.php?data=saisons&idserie=" + id);
 	xmlHttpSaisons.send();
 }
+
+
 
 function getNbSaisons(id, cellule){
 	var nbSaisons = 0;
